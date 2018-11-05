34588b8582043d7edc4c42684638105d7696659b
Annaelle Jaffrelot <annaellejaffrelot@outlook.fr>
2018-11-05 15:59:04 +0100

    footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,3 +1,20 @@
-    <footer> Footer</footer>
-      </body>
-    </html>
+
+	<footer>
+
+			<p>Les évenements à Nantes| <?php echo date("Y") ?></p>
+			<p>
+				<?php 
+					//pas connecté
+					if (empty($_SESSION['pseudo'])) {
+						echo "Vous n'etes pas connectés";
+					}
+					//si connecté !
+					else {
+						echo "Vous êtes connectés en tant que".$_SESSION['users']['pseudo'];
+					}
+				?>
+			</p>
+		</footer>
+	</div>
+</body>
+</html>
