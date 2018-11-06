9e89edc7cad3b1a4492b127504c00325682d6539
Charles <vallais.charles@gmail.com>
2018-11-06 10:58:53 +0100

    Calendar

--- events.php
+++ events.php
@@ -5,31 +5,50 @@ $description = '' ;
 
 
 <?php include_once('layouts/header.php') ?>
+<div class="container">
 
 
 
-<div id='calendar'>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
+
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
+
+  <div id='calendar'>
+
 <script>
-
-$(function() {
-
-  // page is now ready, initialize the calendar...
-
-  $('#calendar').fullCalendar({
-    // put your options and callbacks here
-  })
-
-});
+  $(function() {
+    $('#calendar').fullCalendar({
+      themeSystem: 'bootstrap4',
+      selectable: true,
+      locale: 'fr',
+      timeFormat: '(H:mm)',
+      defaultDate: moment().today,
+      defaultViex: 'month',
+      header: {
+        left: 'title',
+        center: '',
+        right: 'today prev,next month,basicWeek',
+      },
+      buttonText: {
+        today: 'Aujourd\'hui',
+        month: 'Mois',
+        week: 'Semaine',
+        day: 'Jour',
+        list: 'Liste'
+      },
+    });
+  });
 </script>
 
 
-</div>
+    </div>
 
 
-<a class="btn btn-primary"  href="add_event.php">Ajouter un évènement</a>
+    <a class="btn btn-primary"  href="add_event.php">Ajouter un évènement</a>
+  </div>
 </div>
 </main>
+
 
 <?php include_once('layouts/footer.php') ?>
