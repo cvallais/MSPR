65a460f36149c91f2a2c8900fc3ebe6ae39c5fca
Charles <vallais.charles@gmail.com>
2018-11-07 12:54:55 +0100

    Description profil

--- php/register.php
+++ php/register.php
@@ -7,18 +7,19 @@ $password = password_hash($password, PASSWORD_DEFAULT, ['cost'=> 10]);
 $date = date('Y-m-d H:i:s');
 
 //On prépare le sql
-$sql ="INSERT INTO users (firstname, lastname, pseudo, email, city, birth_date, password, date_created)
-VALUES (:firstname, :lastname, :pseudo, :email, :city, :birth_date, :password, :date_created)";
+$sql ="INSERT INTO users (firstname, lastname, pseudo, description, email, city, birth_date, password, date_created)
+VALUES (:firstname, :lastname, :pseudo, :description, :email, :city, :birth_date, :password, :date_created)";
 
 //On prépare les données
 $stmt = $conn->prepare($sql);
-$stmt->bindValue(":firstname",  $_POST['firstname']);
-$stmt->bindValue(":lastname",   $_POST['lastname']);
-$stmt->bindValue(":pseudo",     $_POST['pseudo']);
-$stmt->bindValue(":email",      $_POST['email']);
-$stmt->bindValue(":city",       $_POST['city']);
-$stmt->bindValue(":birth_date", $_POST['birthdate']); 
-$stmt->bindValue(":password",   $password);
+$stmt->bindValue(":firstname",   $_POST['firstname']);
+$stmt->bindValue(":lastname",    $_POST['lastname']);
+$stmt->bindValue(":pseudo",      $_POST['pseudo']);
+$stmt->bindValue(":description", $_POST['description']);
+$stmt->bindValue(":email",       $_POST['email']);
+$stmt->bindValue(":city",        $_POST['city']);
+$stmt->bindValue(":birth_date",  $_POST['birthdate']);
+$stmt->bindValue(":password",    $password);
 $stmt->bindValue(":date_created",  $date);
 
 $stmt->execute();
@@ -31,11 +32,9 @@ $_SESSION['connected'] = false;
 $_SESSION [errors] = [
 	'name' => 'Le nom est trop long',
 	'email' => 'L\'email ,n\' est pas bon',
-'password' => 'Le mot de passe est faux', 
+'password' => 'Le mot de passe est faux',
 ];
 
 //On redirige
 header("Location: ../events.php");
 ?>
-
-
